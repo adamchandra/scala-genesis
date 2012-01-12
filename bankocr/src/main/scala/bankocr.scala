ddef66a62e1a4a42ace96e2ac37e26e107a3a18a
Adam Saunders <adamchandra@gmail.com>
2012-01-12 15:49:19 -0500

    better function composition

--- bankocr/src/main/scala/bankocr.scala
+++ bankocr/src/main/scala/bankocr.scala
@@ -1,7 +1,42 @@
+object Commons {
+  def transpose(aas: List[List[Char]]) : List[List[Char]] = {
+    aas match {
+      case Nil              => Nil
+      case Nil :: xss       => transpose(xss)
+      case (x::xs) :: Nil   => (x :: Nil) :: transpose (xs :: Nil)
+      case (x::xs) :: xss   => (x :: (xss map (_.head))) :: transpose (xs :: (xss map (_.tail)))
+    }
+  }
 
+  def test_transpose() {
+    val m = csv("abc,123,789") map (_.toList)
+    val r = csv("a17,b28,c39") map (_.toList)
+    val mp = transpose(m)
+    assert(mp == r)
+  }
+  // test_transpose()
 
+  def csv = (s:String) => (s.split(",") map (_.trim)).toList
+  def wsv = (s:String) => (s.split(" ") map (_.trim)).toList
+}
+
+import Commons._
 
 object BankOCR {
+  // The haskell solution: 
+
+  // main = interact (\x → parse x ⧺ "\n")
+
+  // parse input = map (digit ∘ transpose ∘ tail) (chunk 4 $ dummy : columns input)
+  //     where digit = fromJust ∘ (flip lookup) dTable
+  //           columns  = transpose ∘ lines
+  //           dummy = "   "
+
+  // dTable = [
+  //            ([" _ ",
+  //              "| |",
+  //              "|_|"], '0'), ...
+
 
   // read in a text block containing lcd-like numerals and print out the
   // the numbers they represent, e.g., 
@@ -16,33 +51,9 @@ object BankOCR {
   | |_|   | |_   _|   |  _| |_|   | |_|  _|
   """.trim.stripMargin
 
-  def transpose(aas: List[List[Char]]) : List[List[Char]] = {
-    try {
-      aas match {
-        case Nil              => Nil
-        case Nil :: xss       => transpose(xss)
-        case (x::xs) :: Nil   => (x :: Nil) :: transpose (xs :: Nil)
-        case (x::xs) :: xss   => (x :: (xss map (_.head))) :: transpose (xs :: (xss map (_.tail)))
-      }
-    } catch {
-      case e: Exception => 
-        error("transpose=\n" + aas.mkString("", "\n", "") + "\n===")
-    }
-  }
-
-  def csv = (s:String) => (s.split(",") map (_.trim)).toList
-  def wsv = (s:String) => (s.split(" ") map (_.trim)).toList
-
-  def test_transpose() {
-    val m = csv("abc,123,789") map (_.toList)
-    val r = csv("a17,b28,c39") map (_.toList)
-    val mp = transpose(m)
-    assert(mp == r)
-  }
-  // test_transpose()
 
   def lines = (s:String) => s.lines.toList
-  def toCharMatrix(s:String): List[List[Char]] = lines(s) map (_.toList)
+  def toCharMatrix: String => List[List[Char]] = s => lines(s) map (_.toList)
 
   def join = (ss:List[Char]) => ss.foldLeft("")((a,s) => a+s)
 
@@ -55,6 +66,7 @@ object BankOCR {
   def fmtMatrixCM: List[List[Char]] => String =
     ccs => fmtMatrix(transpose(ccs))
 
+  // this looks like an arrow fanout/act/fanin operation
   def passThru[A](f: A => Unit)(a:A): A = {
     f(a)
     a
@@ -64,30 +76,16 @@ object BankOCR {
   val prntcm = passThru { m:List[List[Char]] => println(fmtMatrixCM(m)) } _
 
   def numeralMap(numerals: String): Map[String, Int] = {
-    val r = (toCharMatrix _ andThen 
-             transpose andThen 
-             (grouped(4)(_)) andThen 
-             (s => 
-               (s map (_.tail) map (transpose(_)) map fmtMatrix)
-             )
-           )  (numerals)
-
-    val rs = (r.map(_.mkString("")) zip (0 to 9)).toMap
-    // rs.keys.foreach { v => println(v) }
-    rs
+    val groups = toCharMatrix  ∘ transpose ∘ grouped(4) 
+    val numeralStrs = groups(numerals) ∘ (_.tail) ∘ fmtMatrixCM
+    (numeralStrs zip (0 to 9)).toMap
   }
 
-  def decodeNumerals: String => List[Int] = 
+  def parse: String => List[Int] = 
     str => {
-      val nlookup = numeralMap(numerals)
-
-      val rfn = (
-        toCharMatrix _ andThen 
-        transpose andThen 
-        grouped(3) andThen 
-        (gs => gs.map (fmtMatrixCM(_)) map (nlookup(_)))
-      ) 
-      rfn(str)
+      val digit = (s:String) => numeralMap(numerals)(s)
+      val groups = toCharMatrix ∘ transpose ∘ grouped(3) 
+      groups(str) ∘ (fmtMatrixCM ∘ digit)
   }
 
   // later variations - try to correct for noisy input
@@ -106,6 +104,6 @@ object BankOCRApp extends App {
   import BankOCR._
 
   override def main(args: Array[String]) = {
-    println("decodeNumerals() = " + decodeNumerals(testInput).mkString("[", ", ", "]"))
+    println("decodeNumerals() = " + parse(testInput).mkString("[", ", ", "]"))
   }
 }
