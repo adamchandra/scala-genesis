d2bc959da3b8b31fc34d310b5c1f9871cc44732d
Adam Saunders <adamchandra@gmail.com>
2012-01-12 17:27:29 -0500

    various tweaks

--- bankocr/src/main/scala/bankocr.scala
+++ bankocr/src/main/scala/bankocr.scala
@@ -1,12 +1,25 @@
+
+import scalaz._
+import Scalaz._
+
 object Commons {
-  def transpose(aas: List[List[Char]]) : List[List[Char]] = {
+  def transpose[A](aas: List[List[A]]) : List[List[A]] = {
     aas match {
       case Nil              => Nil
       case Nil :: xss       => transpose(xss)
       case (x::xs) :: Nil   => (x :: Nil) :: transpose (xs :: Nil)
-      case (x::xs) :: xss   => (x :: (xss map (_.head))) :: transpose (xs :: (xss map (_.tail)))
+      case (x::xs) :: xss   => (x :: (xss ∘ head)) :: transpose (xs :: (xss ∘ tail))
     }
   }
+
+  // def trans[M[_] : Functor, N[_] : Functor, A](aas: M[N[A]]): M[N[A]] =  {
+  //   aas match {
+  //     case Nil              => Nil
+  //     case Nil :: xss       => trans(xss)
+  //     case (x::xs) :: Nil   => (x :: Nil) :: trans (xs :: Nil)
+  //     case (x::xs) :: xss   => (x :: (xss map (_.head))) :: trans (xs :: (xss map (_.tail)))
+  //   }
+  // }
 
   def test_transpose() {
     val m = csv("abc,123,789") map (_.toList)
@@ -15,6 +28,11 @@ object Commons {
     assert(mp == r)
   }
   // test_transpose()
+
+  def head[A](a:Seq[A]) = a.head
+  def head[A](a:List[A]) = a.head
+  def tail[A](a:Seq[A]) = a.tail
+  def tail[A](a:List[A]) = a.tail
 
   def csv = (s:String) => (s.split(",") map (_.trim)).toList
   def wsv = (s:String) => (s.split(" ") map (_.trim)).toList
@@ -41,9 +59,6 @@ object BankOCR {
   // read in a text block containing lcd-like numerals and print out the
   // the numbers they represent, e.g., 
 
-  import scalaz._
-  import Scalaz._
-
   //123 123 123 123 123 123 123 123 123
   val numerals = """
   |  _       _   _       _   _   _   _   _ 
@@ -51,32 +66,31 @@ object BankOCR {
   | |_|   | |_   _|   |  _| |_|   | |_|  _|
   """.trim.stripMargin
 
+  type CharMatrix = List[List[Char]]
 
   def lines = (s:String) => s.lines.toList
   def toCharMatrix: String => List[List[Char]] = s => lines(s) map (_.toList)
 
-  def join = (ss:List[Char]) => ss.foldLeft("")((a,s) => a+s)
+  def joinChars = (ss:List[Char]) => ss.foldMap(_.toString)
 
+  // there has to be a container-neutral version of this somewhere...
   def grouped[A](n:Int)(ls:List[List[A]]): List[List[List[A]]] = 
     ls.grouped(n).toList
 
   def fmtMatrix: List[List[Char]] => String =
-    ccs => (ccs map (join(_))).mkString("\n")
+    ccs => (ccs ∘ joinChars).mkString("\n")
 
   def fmtMatrixCM: List[List[Char]] => String =
     ccs => fmtMatrix(transpose(ccs))
 
-  // this looks like an arrow fanout/act/fanin operation
-  def passThru[A](f: A => Unit)(a:A): A = {
-    f(a)
-    a
-  }
-  
-  val prnt = passThru { m:List[List[Char]] => println(fmtMatrix(m)) } _
-  val prntcm = passThru { m:List[List[Char]] => println(fmtMatrixCM(m)) } _
+  def passThru[A](f: => A => Unit): A => A = 
+    a => { f(a); a }
+
+  val prnt = passThru(fmtMatrix ∘ println)
+  val prntcm = passThru(fmtMatrixCM ∘ println)
 
   def numeralMap(numerals: String): Map[String, Int] = {
-    val groups = toCharMatrix  ∘ transpose ∘ grouped(4) 
+    val groups = toCharMatrix ∘ transpose ∘ grouped(4) 
     val numeralStrs = groups(numerals) ∘ (_.tail) ∘ fmtMatrixCM
     (numeralStrs zip (0 to 9)).toMap
   }
@@ -88,16 +102,13 @@ object BankOCR {
       groups(str) ∘ (fmtMatrixCM ∘ digit)
   }
 
-  // later variations - try to correct for noisy input
-  // ...and number that have arbitrary spacing, e.g., 
+  // later variations - try to correct for noisy input and number that have arbitrary spacing
 
   val testInput = """
   |    _     _  _  _  _  _  _  _  _ 
   |  | _||_| _| _||_  _||_   | _||_|
   |  ||_   ||_  _| _||_ |_|  ||_ |_|
   """.trim.stripMargin
-
-
 }
 
 object BankOCRApp extends App {
